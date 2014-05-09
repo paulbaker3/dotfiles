3d748d761c85213e2a7f9a78cfc6ecb587908709
pb3 <paul.baker.3@gmail.com>
2014-05-09 12:13:43 -0600

    Grammar

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 
-# Will install Homebrew
+# Install Homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew doctor
 
